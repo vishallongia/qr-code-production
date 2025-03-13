85babce75f414853755dffb5ec65a92750de7179
vishallongia <rahulsaini663@gmail.com>
2025-03-13 21:49:50 +0530

    Changes in progress to show continue with google on register form

--- public/js/login.js
+++ public/js/login.js
@@ -14,19 +14,37 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function toggleMagicLink() {
-  const passwordField = document.getElementById("PasswordText").closest(".input-group");
-  const loginButton = document.getElementById("LoginBtnText");
-  const magicLinkText = document.getElementById("magicLinkText");
+// Function to toggle Magic Link for both login and register
+function toggleMagicLink(formType) {
+  let passwordField, loginButton, magicLinkText;
+
+  if (formType === "login") {
+    passwordField = document
+      .getElementById("PasswordText")
+      .closest(".input-group");
+    loginButton = document.getElementById("LoginBtnText");
+    magicLinkText = document.getElementById("magicLinkText");
+  } else if (formType === "register") {
+    passwordField = document
+      .getElementById("RegisterPasswordText")
+      .closest(".input-group");
+    loginButton = document.getElementById("RegisterBtnText");
+    magicLinkText = document.getElementById("registerMagicLinkText");
+  }
 
   if (passwordField.style.display === "none") {
-    // Switch back to Password Login
+    // Switch back to Password Login/Register
     passwordField.style.display = "flex";
-    loginButton.textContent = "Login";
-    loginButton.setAttribute("onclick", "return handleLogin(event)");
+    loginButton.textContent = formType === "login" ? "Login" : "Register";
+    loginButton.setAttribute(
+      "onclick",
+      formType === "login"
+        ? "return handleLogin(event)"
+        : "return handleRegister(event)"
+    );
     magicLinkText.textContent = "Use Magic Link";
   } else {
-    // Switch to Magic Link Login
+    // Switch to Magic Link Login/Register
     passwordField.style.display = "none";
     loginButton.textContent = "Send Magic Link";
     loginButton.setAttribute("onclick", "return handleMagicLink(event)");
