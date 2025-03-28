dbe7f13896dda8febd3c4a7bd65acaa27c083152
vishallongia <125539993+vishallongia@users.noreply.github.com>
2025-03-28 14:51:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -55,4 +55,5 @@ app.use("/", indexRouter);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Running on Node.js version: ${process.version}`);
 });
