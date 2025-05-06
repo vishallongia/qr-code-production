03d0c3e6b60b48494a6b1a54817dc7eacefb6ac5
vishallongia <rahulsaini663@gmail.com>
2025-05-07 00:27:44 +0530

    new changes

--- config/paypal.js
+++ config/paypal.js
@@ -5,7 +5,7 @@ function environment() {
   let clientSecret = process.env.PAYPAL_CLIENT_SECRET;
 
   if (process.env.NODE_ENV === "production") {
-    return new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret);
+    return new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
   } else {
     return new checkoutNodeJssdk.core.SandboxEnvironment(
       clientId,
