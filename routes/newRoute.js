5c9f5757a28a5039879ab2d100e3d04c69c0adec
vishallongia <rahulsaini663@gmail.com>
2025-06-05 01:16:36 +0530

    New Changes

--- routes/newRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const QRCodeData = require("../models/QRCODEDATA");
-const { authMiddleware } = require("../middleware/auth");
-const SendEmail = require("../Messages/SendEmail");
-
-module.exports = router;
